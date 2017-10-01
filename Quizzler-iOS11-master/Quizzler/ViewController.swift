f166c2801fd208e7ac04cd07567c9240c7567c8b
Christopher Lee <hanernlee@gmail.com>
2017-10-01 13:48:47 +1100

    Incorporating third party libraries in Objective-C

--- Quizzler-iOS11-master/Quizzler/ViewController.swift
+++ Quizzler-iOS11-master/Quizzler/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     let allQuestions = QuestionBank()
     var pickedAnswer : Bool = false
     var questionNumber : Int = 0
+    var score : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -23,8 +24,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstQuestion = allQuestions.list[0]
-        questionLabel.text = firstQuestion.questionText
+        nextQuestion()
     }
 
     @IBAction func answerPressed(_ sender: AnyObject) {
@@ -42,12 +42,17 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
-      
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1) / 13"
+        
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
     }
     
     func nextQuestion() {
         if questionNumber <= 12 {
             questionLabel.text = allQuestions.list[questionNumber].questionText
+            
+            updateUI()
         } else {
             let alert = UIAlertController(title: "Awesome", message: "You have finished all the questions, do you want to start over?", preferredStyle: .alert)
             let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { action  in
@@ -65,13 +70,15 @@ class ViewController: UIViewController {
         let correctAnswer = allQuestions.list[questionNumber].answer
         
         if correctAnswer == pickedAnswer {
-            print("correct")
+            ProgressHUD.showSuccess("Correct!")
+            score += 1
         } else {
-            print("wrong")
+            ProgressHUD.showError("Wrong!")
         }
     }
     
     func startOver() {
+        score = 0
         questionNumber = 0
         nextQuestion()
     }
