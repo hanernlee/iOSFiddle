e9030c6ab315b6853f522c9482312a6a52d698a2
Christopher Lee <hanernlee@gmail.com>
2018-04-09 21:25:39 +1000

    Lifehacks

---
+++ Lifehacks/Lifehacks/ModelController.swift
@@ -0,0 +1,9 @@
+//
+//  ModelController.swift
+//  Lifehacks
+//
+//  Created by Christopher Lee on 9/4/18.
+//  Copyright © 2018 Christopher Lee. All rights reserved.
+//
+
+import Foundation
---
+++ Lifehacks/Lifehacks/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Lifehacks
+//
+//  Created by Christopher Lee on 9/4/18.
+//  Copyright © 2018 Christopher Lee. All rights reserved.
+//
+
+import Foundation
