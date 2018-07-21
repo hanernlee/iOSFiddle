4d574fd08ac1338150e273e20482fc949c8cfba8
Christopher Lee <hanernlee@gmail.com>
2018-07-21 16:35:08 +1000

    Robinhood card swipe

--- iOSControls/Robinhood/Robinhood/Cards/CardStackLayout.swift
+++ iOSControls/Robinhood/Robinhood/Cards/CardStackLayout.swift
@@ -28,15 +28,66 @@
 
 import UIKit
 
+protocol CardStackLayoutDelegate: class {
+    func cardShouldRemove(_ flowLayout: CardStackLayout, indexPath: IndexPath)
+}
+
 class CardStackLayout: UICollectionViewLayout {
   
   private var panGestureRecognizer: UIPanGestureRecognizer = UIPanGestureRecognizer()
+  private let maxOffsetThresholdPercentage: CGFloat = 0.3
+    
+  typealias CellWithIndexPath = (cell: UICollectionViewCell, indexPath: IndexPath)
+    
+  private let animationDuration: TimeInterval = 0.15
+    
+  weak var delegate: CardStackLayoutDelegate?
+    
+  private var topCellWithIndexPath: CellWithIndexPath? {
+    let lastItem = collectionView?.numberOfItems(inSection: 0) ?? 0
+    let indexPath = IndexPath(item: lastItem - 1, section: 0)
+    
+    guard let cell = collectionView?.cellForItem(at: indexPath) else { return nil }
+    
+    return (cell: cell, indexPath: indexPath)
+  }
+    
+  private var bottomCellWithIndexPath: CellWithIndexPath? {
+    guard let numItems = collectionView?.numberOfItems(inSection: 0), numItems > 1 else { return nil }
+    
+    let indexPath = IndexPath(item: numItems - 2, section: 0)
+    guard let cell = collectionView?.cellForItem(at: indexPath) else { return nil }
+    
+    return (cell: cell, indexPath: indexPath)
+  }
     
   override func prepare() {
     super.prepare()
     
     panGestureRecognizer.addTarget(self, action: #selector(handlePan(gestureRecognizer:)))
     collectionView?.addGestureRecognizer(panGestureRecognizer)
+  }
+    
+  override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+    let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+        
+    attributes.frame = collectionView?.bounds ?? .zero
+        
+    var isNotTop = false
+        
+    if let numItems = collectionView?.numberOfItems(inSection: 0), numItems > 0 {
+        isNotTop = indexPath.row != numItems - 1
+    }
+    
+    attributes.alpha = isNotTop ? 0 : 1
+    
+    return attributes
+  }
+    
+  override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+    let indexPaths = indexPathsForElementsInRect(rect)
+    let layoutAttributes = indexPaths.map { layoutAttributesForItem(at: $0) }.filter { $0 != nil }.map { $0! }
+    return layoutAttributes
   }
   
   fileprivate func indexPathsForElementsInRect(_ rect: CGRect) -> [IndexPath] {
@@ -52,29 +103,59 @@ class CardStackLayout: UICollectionViewLayout {
   }
     
   @objc func handlePan(gestureRecognizer: UIPanGestureRecognizer) {
-    print("Panning")
-  }
-
-  override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-    let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+    let translation = gestureRecognizer.translation(in: collectionView)
+    let xOffset = translation.x
+    let xMaxOffset = (collectionView?.frame.width ?? 0) * maxOffsetThresholdPercentage
     
-    attributes.frame = collectionView?.bounds ?? .zero
-    
-    var isNotTop = false
-    
-    if let numItems = collectionView?.numberOfItems(inSection: 0), numItems > 0 {
-        isNotTop = indexPath.row != numItems - 1
+    switch gestureRecognizer.state {
+    case .changed:
+        if let topCard = topCellWithIndexPath {
+            topCard.cell.transform = CGAffineTransform(translationX: xOffset, y: 0)
+        }
+        
+        if let bottomCard = bottomCellWithIndexPath {
+            bottomCard.cell.alpha = 1
+        }
+    case .ended:
+        if abs(xOffset) > xMaxOffset {
+            if let topCard = topCellWithIndexPath {
+                animateAndRemove(left: xOffset < 0, cell: topCard.cell, completion: {
+                    [weak self] in
+                    
+                    guard let `self` = self else { return }
+                    self.delegate?.cardShouldRemove(self, indexPath: topCard.indexPath)
+                })
+            }
+            
+            if let bottomCard = bottomCellWithIndexPath {
+                animateIntoPosition(cell: bottomCard.cell)
+            }
+        } else {
+            if let topCard = topCellWithIndexPath {
+                animateIntoPosition(cell: topCard.cell)
+            }
+        }
+    default:
+        break
     }
-    
-    attributes.alpha = isNotTop ? 0 : 1
-    
-    return attributes
   }
     
-    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        let indexPaths = indexPathsForElementsInRect(rect)
-        let layoutAttributes = indexPaths.map { layoutAttributesForItem(at: $0) }.filter { $0 != nil }.map { $0! }
-        return layoutAttributes
+  private func animateIntoPosition(cell: UICollectionViewCell) {
+    UIView.animate(withDuration: animationDuration) {
+        cell.transform = CGAffineTransform.identity
+        cell.alpha = 1
+    }
+  }
+    
+    private func animateAndRemove(left: Bool, cell: UICollectionViewCell, completion: (() -> ())?) {
+        let screenWidth = UIScreen.main.bounds.width
+        
+        UIView.animate(withDuration: animationDuration, animations: {
+            let xTranslateOffscreen = CGAffineTransform(translationX: left ? -screenWidth : screenWidth, y: 0)
+            cell.transform = xTranslateOffscreen
+        }) { (completed) in
+            completion?()
+        }
     }
 }
 
